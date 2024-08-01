eae22e1fcbff2b688954401332a3e0b53ffa3311
oeur <micaoeur@gmail.com>
2024-07-31 17:22:29 -0700

    changed star to species

--- load_data.py
+++ load_data.py
@@ -8,33 +8,33 @@ def load_simulation_data(simdir, snapnum, species):
     species (str): 'star', 'gas', 'dark' or 'all'
     '''
     part = gizmo.gizmo_io.Read.read_snapshots([species], 'index', snapnum, simulation_directory=simdir, assign_hosts_rotation=True, assign_hosts=True)
-    part['star'].prop('host.distance.principal.cylindrical')
-    rxyz   = part['star'].prop('host.distance.total')
-    Rxy    = part['star'].prop('host.distance.principal.cylindrical')[:,0]
-    Phixy  = part['star'].prop('host.distance.principal.cylindrical')[:,1]
-    Zxy    = part['star'].prop('host.distance.principal.cylindrical')[:,2]
-    Vrxy   = part['star'].prop('host.velocity.principal.cylindrical')[:,0]
-    Vphixy = part['star'].prop('host.velocity.principal.cylindrical')[:,1]
-    Vzxy   = part['star'].prop('host.velocity.principal.cylindrical')[:,2]
+    part[species].prop('host.distance.principal.cylindrical')
+    rxyz   = part[species].prop('host.distance.total')
+    Rxy    = part[species].prop('host.distance.principal.cylindrical')[:,0]
+    Phixy  = part[species].prop('host.distance.principal.cylindrical')[:,1]
+    Zxy    = part[species].prop('host.distance.principal.cylindrical')[:,2]
+    Vrxy   = part[species].prop('host.velocity.principal.cylindrical')[:,0]
+    Vphixy = part[species].prop('host.velocity.principal.cylindrical')[:,1]
+    Vzxy   = part[species].prop('host.velocity.principal.cylindrical')[:,2]
 
-    x  = part['star'].prop('host.distance.principal.cartesian')[:,0]
-    y  = part['star'].prop('host.distance.principal.cartesian')[:,1]
-    z  = part['star'].prop('host.distance.principal.cartesian')[:,2]
-    vx = part['star'].prop('host.velocity.principal.cartesian')[:,0]
-    vy = part['star'].prop('host.velocity.principal.cartesian')[:,1]
-    vz = part['star'].prop('host.velocity.principal.cartesian')[:,2]
-    ax  = part['star'].prop('host.acceleration.principal.cartesian')[:,0]
-    ay  = part['star'].prop('host.acceleration.principal.cartesian')[:,1]
-    az  = part['star'].prop('host.acceleration.principal.cartesian')[:,2]
+    x  = part[species].prop('host.distance.principal.cartesian')[:,0]
+    y  = part[species].prop('host.distance.principal.cartesian')[:,1]
+    z  = part[species].prop('host.distance.principal.cartesian')[:,2]
+    vx = part[species].prop('host.velocity.principal.cartesian')[:,0]
+    vy = part[species].prop('host.velocity.principal.cartesian')[:,1]
+    vz = part[species].prop('host.velocity.principal.cartesian')[:,2]
+    ax  = part[species].prop('host.acceleration.principal.cartesian')[:,0]
+    ay  = part[species].prop('host.acceleration.principal.cartesian')[:,1]
+    az  = part[species].prop('host.acceleration.principal.cartesian')[:,2]
 
-    age  = part['star'].prop('age')
-    mass = part['star'].prop('mass')
-    feh  = part['star'].prop('metallicity.fe')
-    mgfe = part['star'].prop('metallicity.mg - metallicity.fe')
+    age  = part[species].prop('age')
+    mass = part[species].prop('mass')
+    feh  = part[species].prop('metallicity.fe')
+    mgfe = part[species].prop('metallicity.mg - metallicity.fe')
 
-    pos_stars = part['star'].prop("host.distance.principal") #stellar positions with respect to the host. 
-    pos = part['star'].prop('host.distance.principal.cylindrical')
-    dist_stars = part['star'].prop("host.distance.total") #stellar distance with respect to the host.
-    vel_stars = part['star'].prop("host.velocity.principal")
-    vels = part['star'].prop('host.velocity.principal.cylindrical')
+    pos_stars = part[species].prop("host.distance.principal") #stellar positions with respect to the host. 
+    pos = part[species].prop('host.distance.principal.cylindrical')
+    dist_stars = part[species].prop("host.distance.total") #stellar distance with respect to the host.
+    vel_stars = part[species].prop("host.velocity.principal")
+    vels = part[species].prop('host.velocity.principal.cylindrical')
     return rxyz, Rxy, Phixy, Zxy, Vrxy, Vphixy, Vzxy, x, y, z, vx, vy, vz, ax, ay, az, age, mass, feh, mgfe, pos_stars, pos, dist_stars, vel_stars, vels
